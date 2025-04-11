6b59ba6d5dc83c905a0fa12f21a49eb487af079c
Etrav05 <Etravis090705@gmail.com>
2025-04-11 13:23:52 -0400

    - Sync

--- Group_6_Lab_8_9_Evan_Cole_Liv/Exception_SampleInput.cpp
+++ Group_6_Lab_8_9_Evan_Cole_Liv/Exception_SampleInput.cpp
@@ -6,12 +6,12 @@ void acceptSampleData(double& i) {
     Character_Detected cd;
     Greater_Or_Less_Than_Range glr;
 
-    string input;
+    string input; // accept input as a string 
     cin >> input;
 
-    for (char c : input) {                                      // now for all the characters in this input
-        if (!isdigit(c) && c != '-' && c != '.' && c != ',') { // check if theya re a digit or an acceptable char (. , or -)
-            throw cd;
+    for (char c : input) {                          // now for all the characters in this input
+        if (!isdigit(c) && c != '-' && c != '.') { // check if they are a digit or an acceptable char (. or -) or a digit
+            throw cd;                             // if not, throw exceptions
         }
 
         else
